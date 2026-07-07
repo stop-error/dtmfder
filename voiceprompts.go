8dcb182d05d13b000a783cf57d42dfca37248a15
stop-error <tsnm61tt@duck.com>
2026-07-07 15:33:11 -0400

    fix greeting grammer

--- voiceprompts.go
+++ voiceprompts.go
@@ -11,10 +11,10 @@ var ivrToContinue = &twiml.VoiceSay{
 }
 
 var ivrGreeting1 = &twiml.VoiceSay{
-	Message: `Welcome the Jack dating interactive survey experience.
-	In a moment, you will be asked a brief series of questions to 
-	help determine your compatibility in a potential relationship with Jack.
-	Please use the keypad on your phone to answer each question.
-	Your responses will not be recorded until the end of the survey experence.
-	You may hang up at any time to end the survey and erase all survey answers.`,
+	Message: `Welcome to the Jack dating interactive survey experience.
+    In a moment, you will be asked a brief series of questions to 
+    help determine your compatibility in a potential relationship with Jack.
+    Please use the keypad on your phone to answer each question.
+    Your responses will not be recorded until the end of the survey experience.
+    You may hang up at any time to end the survey and erase your survey responses.`,
 }
